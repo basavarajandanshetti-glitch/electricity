4ed56cd47726ee1da95a1eba87171b477736a1c4
basavarajandanshetti-glitch <basavarajandanshetti@gmail.com>
2025-11-27 15:22:14 +0530

    Update electro.py

--- electro.py
+++ electro.py
@@ -1,12 +1,22 @@
+# Electricity Bill Calculator Program
+
 import sys
+
+# If units are provided as command-line argument
 if len(sys.argv) == 2:
     units = float(sys.argv[1])
     print("User provided units")
 else:
+    # Default units
     units = 100
     print("Default units used")
+
+# Rate per unit
 rate = 5
+
+# Calculate bill
 bill = units * rate
 
 print("Units Consumed:", units)
 print("Electricity Bill (₹):", bill)
+
